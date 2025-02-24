b4ea458a9fb2fef0433df39eec8d649b068114e6
Mehdi <muhammad-ahsan.mehdi.ext@siemens.com>
2025-02-24 23:52:59 +0500

    login functions page done

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -11,3 +11,19 @@ class LoginPage (BasePage):
     password_input = (By.ID, "password")
     login_button = (By.ID, "login-button")
     dashboard_page = (By.ID, "")
+
+    # calling the constructor
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    # interacting with the username element
+    def enter_username (self, username):
+        self.entering_text_element(self.usename_input, username)
+    
+    # interacting with password element
+    def enter_password(self, password):
+        self.entering_text_element(self.password_input, password)
+
+    # now clicking on the login button
+    def click_on_element(self, element_locator):
+        self.click_on_element
